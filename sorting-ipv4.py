0597fe21f7b22a4eef8e4c3c4107b76ac0225d9e
Georgios Smaragdakis <gsmaragd@mit.edu>
2016-08-12 14:20:54 +0200

    updates by pr

--- sorting-ipv4.py
+++ sorting-ipv4.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python
 
 import sys
+from collections import defaultdict
 
-list_of_ips = {}
+counts = defaultdict(int)
+
+
+#list_of_ips = {}
+
+list_of_ips = set()
+
 
 for line in sys.stdin:
-	line = line.strip()
-	list_of_ips[line]=line
-	#print line
+    line = line.strip()
+    #list_of_ips[line] = line
+    list_of_ips.add(line)
+    counts[line] += 1
+    print line
 
-#print "++++"
+print "++++"
 
 s = sorted(list_of_ips, key=lambda ip: long(''.join(["%02X" % long(i) for i in ip.split('.')]), 16))
 
 for key in s:
-	print "%s" % key
+    print str(key) + " " + str(counts[key])
 
 
